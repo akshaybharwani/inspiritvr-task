2199261d67e76c44f4a890736caafbe022a31f43
Akshay Bharwani <akshaybharwani@pm.me>
2020-07-26 17:45:26 +0530

    Add Create Rooms logic

---
+++ InspiritVRTask/Assets/_Scripts/UI/CreateRoom.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using UnityEngine;
+
+public class CreateRoom : MonoBehaviourPunCallbacks
+{
+    #region Private Variables
+
+    [SerializeField] 
+    private TextMeshProUGUI roomName;
+
+    #endregion
+
+    public void onClick_CreateRoom()
+    {
+        if (!PhotonNetwork.IsConnected)
+            return;
+        
+        RoomOptions roomOptions = new RoomOptions {MaxPlayers = 4};
+
+        if (roomName)
+            PhotonNetwork.JoinOrCreateRoom(roomName.text, roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Created Room successfully");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Room creation failed " + message);
+    }
+}
